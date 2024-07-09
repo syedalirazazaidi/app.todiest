2887d3dc502ed409e8d926e9800ad3ca56e814f2
syedalirazazaidi <aaliraza007@hotmail.com>
2024-07-09 17:19:12 +0500

    changes css

---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className=" ">
+      this is ali raza developer
+      <Button variant='destructive'>Click ,e</Button>
+
+    </main>
+  ); 
+}
