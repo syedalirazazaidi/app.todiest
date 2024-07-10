9281d9ab46bc81cccedc13f974c81385c0452df1
syedalirazazaidi <aaliraza007@hotmail.com>
2024-07-10 16:58:47 +0500

    add navbar types

---
+++ frontend/constants/data.ts
@@ -0,0 +1,34 @@
+import { NavItem } from "@/types";
+
+export const navItems: NavItem[] = [
+  {
+    title: "Inbox",
+    href: "/inbox",
+    icon: "inbox",
+    label: "inbox",
+    description: "inbox",
+  },
+  {
+    title: "Today",
+    href: "/today",
+    icon: "today",
+    label: "today",
+    description: "/today",
+  },
+
+  {
+    title: "Upcoming",
+    href: "/upcoming",
+    icon: "upcoming",
+    label: "upcoming",
+    description: "/today",
+  },
+
+  {
+    title: "Filters & Lables",
+    href: "/filterandlabels",
+    icon: "filterandlabels",
+    label: "filterandlabels",
+    description: "/today",
+  },
+];
---
+++ frontend/types/index.ts
@@ -0,0 +1,9 @@
+export interface NavItem {
+  title: string;
+  href?: string;
+  disabled?: boolean;
+  external?: boolean;
+  label?: string;
+  description?: string;
+  icon?: string;
+}
