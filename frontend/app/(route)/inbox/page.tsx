8dadbeae98268b8636fca5ea4441f2afcc19de94
syedalirazazaidi <aaliraza007@hotmail.com>
2024-07-17 20:09:36 +0500

    format code

--- frontend/app/(route)/inbox/page.tsx
+++ frontend/app/(route)/inbox/page.tsx
@@ -6,10 +6,13 @@ export default function page() {
       <h1 className="mb-4 text-2xl font-bold leading-none tracking-tight text-gray-900 md:text-2xl lg:text-2xl dark:text-white">
         Inbox
       </h1>
-      <h1 className="mb-4 text-1xl font-semibold leading-none tracking-tight text-gray-900 md:text-1xl lg:text-1xl dark:text-white">
+      <h1 className=" text-1xl font-semibold leading-none tracking-tight text-gray-900 md:text-1xl lg:text-1xl dark:text-white">
         Priority 1
       </h1>
+      <hr className="h-px my-2 bg-gray-200 border-0 dark:bg-gray-700" />
+
       <p>
+        <span className="w-5 h-5 rounded-full bg-red-900 border-1 border-amber-400	 inline-block"></span>
         convallis, lorem enim condimentum mauris, eu dictum enim sapien eget
       </p>
     </div>
