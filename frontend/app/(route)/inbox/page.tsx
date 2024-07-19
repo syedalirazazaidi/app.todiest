a289780ecbd7d40bb87ad474dd90e4ca13e9047f
syedalirazazaidi <aaliraza007@hotmail.com>
2024-07-19 17:12:36 +0500

    add css

--- frontend/app/(route)/inbox/page.tsx
+++ frontend/app/(route)/inbox/page.tsx
@@ -34,7 +34,10 @@ export default function page() {
         <div>
           {isHovered && (
             <div className="flex gap-2">
-              <Pencil className="w-5 h-5 text-gray-500" />
+              <Pencil
+                className="w-5 h-5 text-gray-500"
+                onClick={() => alert("this is ")}
+              />
               <Calendar className="w-5 h-5 text-gray-500" />
               <SquareCheckBig className="w-5 h-5 text-gray-500" />
               <EllipsisVertical className="w-5 h-5 text-gray-500" />
