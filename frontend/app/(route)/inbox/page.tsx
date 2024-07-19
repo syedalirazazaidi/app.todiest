8395058ac3ed15dd41aa8d95c042a181139a65f8
syedalirazazaidi <aaliraza007@hotmail.com>
2024-07-19 16:01:30 +0500

    add new icons

--- frontend/app/(route)/inbox/page.tsx
+++ frontend/app/(route)/inbox/page.tsx
@@ -1,7 +1,12 @@
 "use client";
 
 import React, { useState, useEffect } from "react";
-import { Pencil } from "lucide-react";
+import {
+  Pencil,
+  Calendar,
+  SquareCheckBig,
+  EllipsisVertical,
+} from "lucide-react";
 
 export default function page() {
   const [isHovered, setIsHovered] = useState(false);
@@ -19,7 +24,6 @@ export default function page() {
       <div
         className="flex justify-between items-center gap-1 cursor-pointer
         "
-        onClick={() => alert("hi developer")}
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
@@ -27,7 +31,16 @@ export default function page() {
           <span className="w-5 h-5 rounded-full bg-amber-100 border-[2px] border-amber-600 inline-block"></span>
           convallis, lorem enim condimentum mauris,
         </p>
-        <div>{isHovered && <Pencil className="w-5 h-5 text-amber-600" />}</div>
+        <div>
+          {isHovered && (
+            <div className="flex gap-2">
+              <Pencil className="w-5 h-5 text-gray-500" />
+              <Calendar className="w-5 h-5 text-gray-500" />
+              <SquareCheckBig className="w-5 h-5 text-gray-500" />
+              <EllipsisVertical className="w-5 h-5 text-gray-500" />
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
